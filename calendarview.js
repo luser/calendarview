a83367660c445211d2d2f4142aaf394dd5be5adb
Ted Mielczarek <ted@mielczarek.org>
2016-07-18 12:59:00 -0400

    refactor ical event source a bit

--- calendarview.js
+++ calendarview.js
@@ -1,4 +1,4 @@
-/*global $ */
+/*global $,ical_event_source */
 $(document).ready(function() {
   $('#calendar').fullCalendar({
     header: {
@@ -6,16 +6,29 @@ $(document).ready(function() {
       center: 'title',
       right: ''
     },
-    defaultView: 'agendaWeek',
-    timezone: 'local'
+    // I don't like how the week agenda shows days that
+    // are already past.
+    defaultView: 'agendaSixDay',
+    timezone: 'local',
+    views: {
+      agendaSixDay: {
+        type: 'agenda',
+        duration: { days: 6 },
+        buttonText: '6 day'
+      }
+    }
   });
   // for each ICAL
   // addICALSource(url)
 });
 
+//TODO: add a way to add ICAL sources that is saved in IDB or something
 //TODO: periodically poll to set the current date
 // $('#calendar').fullCalendar('today');
+//TODO: periodically refresh calendar sources
+// $('#calendar').fullCalendar('refetchEvents')
+
 
 function addICALSource(url) {
-  $('#calendar').fullCalendar('addEventSource', {id: url, events: ical_source(url) });
+  $('#calendar').fullCalendar('addEventSource', ical_event_source(url));
 }
--- icalsource.js
+++ icalsource.js
@@ -1,66 +1,52 @@
 /*global ICAL,$ */
+// Return a Promise that resolves to jCal data of the ICAL file at `url`.
 function get_ical_data(url) {
   return new Promise((resolve, reject) => {
     var req = new XMLHttpRequest();
     req.onreadystatechange = (ev) => {
-      if (req.readyState == 4 && req.status == 200) {
-        resolve(ICAL.parse(req.responseText));
+      if (req.readyState == 4) {
+        if (req.status == 200) {
+          resolve(ICAL.parse(req.responseText));
+        } else {
+          reject("Error, HTTP " + req.status.toString());
+        }
       }
     };
+    req.onerror = e => reject(e);
     req.open("GET", url, true);
     req.send(null);
   });
 }
 
-function ICALSource(url) {
-  this.url = url;
-  this.events = [];
-  this.event_promise = new Promise((resolve, reject) => {
-    get_ical_data(url).then(data => {
-      var comp = new ICAL.Component(data);
-      this.events = comp.getAllSubcomponents("vevent").map(ve => new ICAL.Event(ve));
-      this.event_promise = null;
-      resolve(this.events);
-    });
-  });
-}
-
-ICALSource.prototype = {
-  get_events: function() {
-    if (this.event_promise == null) {
-      return Promise.resolve(this.events);
-    } else {
-      return this.event_promise;
-    }
-  }
-};
-
-function ical_source(url) {
-  var source = new ICALSource(url);
-  return function(start, end, timezone, callback) {
-    source.get_events().then(source_events => {
-      console.log('Got %d calendar events', source_events.length);
-      var events = [];
-      for (var entry of source_events) {
-        if (entry.isRecurring())
+// Return an EventSource that fetches events from the ICAL file at `url`.
+function ical_event_source(url) {
+  return {
+    id: url,
+    events: (start, end, timezone, callback) => {
+      get_ical_data(url).then(data => {
+        var comp = new ICAL.Component(data);
+        var events = comp.getAllSubcomponents('vevent').map(ve => new ICAL.Event(ve));
+        var color = comp.getFirstPropertyValue('x-apple-calendar-color');
+        console.log('Got %d calendar events', events.length);
+        callback(
+          events
           //TODO: handle recurring events
-          continue;
-        if ($.fullCalendar.moment(entry.startDate.toJSDate()).isBetween(start, end, null, '[]') || $.fullCalendar.moment(entry.endDate.toJSDate()).isBetween(start, end, null, '[]')) {
-          console.log('Adding event %s', entry.summary);
-          events.push({
-            id: entry.uid,
-            title: entry.summary,
-            allDay: entry.startDate.isDate,
-            start: entry.startDate.toJSDate(),
-            end: entry.endDate.toJSDate(),
-            url: url,
-            location: entry.location,
-            description: entry.description
-          });
-        }
-      }
-      callback(events);
-    });
+            .filter(entry => !entry.isRecurring() && ($.fullCalendar.moment(entry.startDate.toJSDate()).isBetween(start, end, null, '[]') || $.fullCalendar.moment(entry.endDate.toJSDate()).isBetween(start, end, null, '[]')))
+            .map(entry => {
+              return {
+                id: entry.uid,
+                title: entry.summary,
+                allDay: entry.startDate.isDate,
+                start: entry.startDate.toJSDate(),
+                end: entry.endDate.toJSDate(),
+                url: url,
+                color: color,
+                location: entry.location,
+                description: entry.description
+              };
+            }));
+      });
+    }
   };
 }
 
